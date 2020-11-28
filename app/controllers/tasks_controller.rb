af074fac9918d31faacb0b6a5f341246cb82eb01
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-11-28 12:34:19 -0800

    Add task show action and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,26 @@
 class TasksController < ApplicationController
     #show list of tasks
     def index
-        @tasks = Task.all 
+        @tasks = Task.order(created_at: :desc)
     end
+
+    def new
+        @task = Task.new 
+    end
+    
+    def create
+        @task = Task.new(params.require(:task).permit(:title))
+        if @task.save
+            redirect_to tasks_path
+        else
+            redirect_to new_task_path
+        end
+    end
+
+    def show
+        id = params[:id]
+        @task = Task.find(id)
+    end
+   
 
 end
